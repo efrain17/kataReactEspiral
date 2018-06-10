0516b01cbbe47cf21c82e381fca3ad89f4e423f6
efrain <carlosefrain17@hotmail.com>
2018-06-09 21:04:50 -0500

    kT-1 recorrido de la tabla

--- src/Exercise1/Exercise1.js
+++ src/Exercise1/Exercise1.js
@@ -5,6 +5,99 @@ export class Exercise1 extends Component {
     super(props);
     this.state = {};
   }
+
+  generarMatriz() {
+    return [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [5, 4, 5]
+    ];
+  }
+
+  generarMatrizDos() {
+    return [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ];
+  }
+
+  generarSuperMatriz() {
+    return [
+      [ 1,  2,  3,  4,  5,  6],
+      [22, 23, 24, 25, 26,  7],
+      [21, 36, 37, 38, 27,  8],
+      [20, 35, 42, 39, 28,  9],
+      [19, 34, 41, 40, 29, 10],
+      [18, 33, 32, 31, 30, 11],
+      [17, 16, 15, 14, 13, 12]
+    ];
+  }
+
+  generarMatrizTres() {
+    return [
+      [ 1,  2,  3,  4,  5,  6],
+      [18, 19, 20, 21, 22,  7],
+      [17, 28, 29, 30, 23,  8],
+      [16, 27, 26, 25, 24,  9],
+      [15, 14, 13, 12, 11, 10]
+    ];
+  }
+
+  getNumeroIteraciones(matris) {
+    return matris[0].length * matris.length;
+  }
+
+  getRecorrido(matris, numeroIteraciones) {
+    let resultado = [];
+    let fila = 0;
+    let columna = 0;
+    let maxFila = matris.length- 1;
+    let minFila = 0;
+
+    let minColumna = 0;
+    let maxColumna = matris[0].length -1;
+    let estado = null;
+
+    for (var i = 0; i < numeroIteraciones; i++) {
+      if(matris[fila] && matris[0][columna]) {
+        resultado.push(matris[fila][columna]);
+      }
+      estado = null;
+      if(fila > minFila  && columna == minColumna) {
+        fila--;
+        estado = 'Subir';
+      }
+      else if(fila == minFila && columna < maxColumna) {
+        columna ++;
+        estado = 'Derecha';
+      }
+      else if(fila == maxFila && columna >= minColumna) {
+        columna --;
+        estado = 'Izquierda';
+      }
+      else if(columna == maxColumna && fila <= maxFila) {
+        fila ++;
+        estado = 'Bajar';
+      }
+
+      if(columna == maxColumna && fila == maxFila && estado == 'Bajar') {
+        maxColumna = maxColumna - 1;
+      }
+      if(columna == minColumna && fila == minFila && estado == 'Subir') {
+        minColumna = minColumna + 1;
+      }
+      if(fila == minFila && columna == maxColumna && estado == 'Derecha') {
+        minFila = minFila +1;
+      }
+      if(fila == maxFila && columna == minColumna && estado == 'Izquierda') {
+        maxFila = maxFila - 1;
+      }
+    } 
+    return resultado;
+  }
+  
   
   render() {
     return (
--- src/Exercise1/specs/Exercise1.spec.js
+++ src/Exercise1/specs/Exercise1.spec.js
@@ -15,5 +15,90 @@ describe('Exercise1', () => {
     it('should defined state', () => {
       expect(instance.state).toEqual({});
     });
+
+    it('Matriz generada', () => {
+      let matris = instance.generarMatriz();
+      expect(matris[0]).toEqual([1, 2, 3]);
+      expect(matris[1]).toEqual([4, 5, 6]);
+      expect(matris[2]).toEqual([7, 8, 9]);
+    });
+
+    it('Matriz generada Dos', () => {
+      let matris = instance.generarMatrizDos();
+      expect(matris[1]).toEqual([4, 5, 6]);
+    });
+
+    it('Matriz generada Tres', () => {
+      let matris = instance.generarMatrizTres();
+      let matrisCorrecta = [
+        [ 1,  2,  3,  4,  5,  6],
+        [18, 19, 20, 21, 22,  7],
+        [17, 28, 29, 30, 23,  8],
+        [16, 27, 26, 25, 24,  9],
+        [15, 14, 13, 12, 11, 10]
+      ];
+      expect(matris).toEqual(matrisCorrecta);
+    });
+
+    it('Super matriz generada', () => {
+      let matris = instance.generarSuperMatriz();
+      let matrisCorrecta = [
+        [ 1,  2,  3,  4,  5,  6],
+        [22, 23, 24, 25, 26,  7],
+        [21, 36, 37, 38, 27,  8],
+        [20, 35, 42, 39, 28,  9],
+        [19, 34, 41, 40, 29, 10],
+        [18, 33, 32, 31, 30, 11],
+        [17, 16, 15, 14, 13, 12]
+      ];
+      expect(matris).toEqual(matrisCorrecta);
+    });
+
+    it('Retornar numero iteraciones', () => {
+      let matris = instance.generarMatriz();
+      let numeroIteraciones = instance.getNumeroIteraciones(matris);
+      expect(numeroIteraciones).toEqual(12);
+    });
+
+    it('Obtener resultado Uno', () => {
+      let matris = instance.generarMatriz();
+      let numeroIteraciones = instance.getNumeroIteraciones(matris);
+      let resultado = instance.getRecorrido(matris, numeroIteraciones);
+      expect(resultado).toEqual([1, 2, 3, 6, 9, 5, 4, 5, 7, 4, 5, 8]);
+    });
+
+    it('Obtener resultado Tres', () => {
+      let matris = instance.generarMatrizTres();
+      let numeroIteraciones = instance.getNumeroIteraciones(matris);
+      let resultado = instance.getRecorrido(matris, numeroIteraciones);
+      let correcto = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30
+      ];
+      expect(resultado).toEqual(correcto);
+    });
+
+    it('Obtener resultado Dos', () => {
+      let matris = instance.generarMatrizDos();
+      let numeroIteraciones = instance.getNumeroIteraciones(matris);
+      let resultado = instance.getRecorrido(matris, numeroIteraciones);
+      expect(resultado).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
+    });
+
+    it('Obtener resultado Super matriz', () => {
+      let matris = instance.generarSuperMatriz();
+      let numeroIteraciones = instance.getNumeroIteraciones(matris);
+      let resultado = instance.getRecorrido(matris, numeroIteraciones);
+      let resultadoCorrecto = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+        11, 12, 13, 14, 15, 16, 17, 18, 
+        19, 20, 21, 22, 23, 24, 25, 26, 
+        27, 28, 29, 30, 31, 32, 33, 34, 
+        35, 36, 37, 38, 39, 40, 41, 42
+      ];
+      expect(resultado).toEqual(resultadoCorrecto);
+    });
+
   });
 });
